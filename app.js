79c67c3134131eca2ac5573b9a220f1924fef06b
agrim08 <agrimgupta0805@gmail.com>
2024-10-28 20:32:49 +0530

    importing json file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("./require.js");
 const { calculateSum, calculateDifference } = require("./sum.js");
+const data = require("./data.json");
 //* import { calculateDifference, calculateSum } from "./sum.js";
 // whatever we export from sum.js will be returned from require method
 
 var name = "Agrim";
-
 var a = 10;
 var b = 20;
+
+console.log(data);
 
 calculateSum(a, b); // you cannot access variable and methods directly by require()
 calculateDifference(a, b);
