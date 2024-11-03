05358a7e65eae94bf28d0dbd81ce91812a260bd4
agrim08 <agrimgupta0805@gmail.com>
2024-11-04 01:14:52 +0530

    server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
+  if (req.url === "/secretdata") {
+    res.end("no secret data available");
+  }
   res.end("hello world"); //saying hello world to every user requesting our server
 });
 
